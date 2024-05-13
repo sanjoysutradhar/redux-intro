05d159aa4648b8d42fc31b2b3b9e5c9d085a2490
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-13 12:10:56 +0600

    professional redux file structure: state

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 300 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
