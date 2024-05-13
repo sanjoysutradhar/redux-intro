015abcde0cd0674e7d3143ef85a0326ad0215aa4
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-13 15:47:45 +0600

    the redux devtool

--- src/store.js
+++ src/store.js
@@ -3,10 +3,14 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accontSlice";
 import customerReducer from "./features/customers/customerSlice";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
