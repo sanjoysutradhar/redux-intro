109cf590f270a82932b1011b6285832446a6c736
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-05-13 10:21:01 +0600

    creating reducer, redux store, working with action creators

---
+++ src/store.js
@@ -0,0 +1,62 @@
+import { createStore } from "redux";
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdraw":
+      //   if (action.payload < state.balance) return;
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return { ...state, loan: 0, balance: state.balance - state.loan };
+
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 300 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(300));
+store.dispatch(requestLoan(1000, "Buy a car"));
+store.dispatch(payLoan());
+console.log(store.getState());
